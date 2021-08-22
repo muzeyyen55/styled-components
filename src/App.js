23590ba36f40958645d5fb116af729914733b68e
Müzeyyen Nur Akçay <77547103+muzeyyen55@users.noreply.github.com>
2021-08-22 22:22:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { StyledButton } from "./components/Button.style";
+import { AppContainer } from "./components/Container.style";
+import { GlobalStyles } from "./GlobalStyles.style";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <GlobalStyles />
+      <StyledButton
+        buttonLabel="Click Here"
+        backgroundColor="violet"
+      ></StyledButton>
+    </AppContainer>
   );
 }
 
